b4401b8f425165501c39e4dbd4ad9ef962b78007
GitHub Action <action@github.com>
2026-02-28 19:37:29 +0000

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ic_python_logging",
-    version="0.3.0",
+    version="0.3.1",
     author="Smart Social Contracts",
     author_email="smartsocialcontracts@gmail.com",
     description="A robust logging system for Internet Computer canisters",
