8b7b45d7a690fda9d4772c4953690a24ef9b08cd
deucalioncodes <jmpmonteagudo@gmail.com>
2026-04-11 09:30:16 +0200

    chore: rename distribution name from ic_python_logging to ic-python-logging for consistency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="ic_python_logging",
+    name="ic-python-logging",
     version="0.3.3",
     author="Smart Social Contracts",
     author_email="smartsocialcontracts@gmail.com",
